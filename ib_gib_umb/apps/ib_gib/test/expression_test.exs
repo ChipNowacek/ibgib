e1896c2335bfc3e54f5fd0fe40dd4c64961375d5
William Raiford <bill.mybiz@gmail.com>
2016-07-18 07:44:55 -0500

    Hello World test implemented and passing!
    * Still need to do more functions on Expression to simplify some
      use cases.
      * Instance/Abstract
      * New Property
    
    * Still not sure how to resolve if multiple timelines occur for any
      given `ib`. I mention this here, because the Hello World instance
      actually references the `text` ib **before** it mut8s to include
      the `Hello World!` text. So, if we had a UI talking with the
      `Hello World` instance, it would know that it had a `text` prop,
      but it would need to "play" the `text` property further along.
      This may become a moot point once I have incorporated a "user"/
      "execution space" that references both, which may indeed be the
      "Thing" on the UI that is "talking" with the HW instance. Need to
      think on it.

--- ib_gib_umb/apps/ib_gib/test/expression_test.exs
+++ ib_gib_umb/apps/ib_gib/test/expression_test.exs
@@ -376,6 +376,48 @@ defmodule IbGib.ExpressionTest do
     assert text_instance_rel8d_info[:relations]["prop_of"] === [hw_instance_ib_gib]
   end
 
+  # @tag :capture_log
+  test "create expression, from scratch, hello world instance Thing with hello world text Thing" do
+    {:ok, root} = Expression.Supervisor.start_expression()
+
+    # Randomized to keep unit tests from overlapping.
+    Logger.debug "gonna hw"
+    hw_ib = "hw_#{RandomGib.Get.some_letters(5)}"
+    {:ok, hw} = root |> Expression.fork(hw_ib)
+
+    Logger.debug "gonna instance hw"
+    hw_instance_ib = "hw instance_#{RandomGib.Get.some_letters(5)}"
+    {:ok, {hw_instance, hw_instance_info, hw_instance_ib_gib}} =
+      hw |> Expression.gib(:fork, hw_instance_ib)
+
+    Logger.debug "gonna text"
+    # Randomized to keep unit tests from overlapping.
+    text_ib = "text_#{RandomGib.Get.some_letters(5)}"
+    {:ok, text} = root |> Expression.fork(text_ib)
+    # text_info = text_thing |> Expression.get_info!
+    # text_ib_gib = Helper.get_ib_gib!(text_info[:ib], text_info[:gib])
+
+    Logger.debug "gonna instance text"
+    text_instance_ib = "text instance_#{RandomGib.Get.some_letters(5)}"
+    {:ok, {text_instance, text_instance_info, text_instance_ib_gib}} =
+      text |> Expression.gib(:fork, text_instance_ib)
+
+    Logger.debug "gonna rel8 'text property'"
+    {
+      :ok,
+      {hw_instance, hw_instance_info, hw_instance_ib_gib},
+      {text_instance, text_instance_info, text_instance_ib_gib}
+    } =
+      hw_instance |> Expression.gib(:rel8, text_instance, ["prop", "text"], ["prop_of"])
+
+    {:ok, {text_instance, text_instance_info, text_instance_ib_gib}} =
+      text_instance
+      |> Expression.gib(:mut8, %{"content" => "Hello World!"})
+
+    Logger.debug "hw_instance_info: #{inspect hw_instance_info}"
+    Logger.debug "text_instance_info: #{inspect text_instance_info}"
+  end
+
   @tag :capture_log
   test "fork via gib" do
 
@@ -437,9 +479,4 @@ defmodule IbGib.ExpressionTest do
     assert new_a_ib_gib === Helper.get_ib_gib!(new_a_info[:ib], new_a_info[:gib])
     assert new_b_ib_gib === Helper.get_ib_gib!(new_b_info[:ib], new_b_info[:gib])
   end
-
-  #
-  # test "create expression, from scratch, hello world instance Thing with hello world text Thing" do
-  #   flunk("not implemented")
-  # end
 end
