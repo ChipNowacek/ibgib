360ae15c5255c0be432a453e97b361f2611a4b62
William Raiford <bill.mybiz@gmail.com>
2016-08-13 16:42:54 -0500

    Implemented edit on client side.

--- ib_gib_umb/apps/web_gib/web/static/js/app.js
+++ ib_gib_umb/apps/web_gib/web/static/js/app.js
@@ -23,56 +23,85 @@ import "phoenix_html"
 class App {
 
   static init(){
+
     // let socket = new Socket("/socket", {
     //   logger: ((kind, msg, data) => { console.log(`${kind}: ${msg}`, data) })
     // })
 
     // socket.connect({user_id: "123"})
-    var $status    = $("#status")
-    var $messages  = $("#messages")
-    var $input     = $("#message-input")
-    var $username  = $("#username")
 
-    socket.onOpen( ev => console.log("OPEN", ev) )
-    socket.onError( ev => console.log("ERROR", ev) )
-    socket.onClose( e => console.log("CLOSE", e))
-
-    var chan = socket.channel("rooms:lobby", {})
-    chan.join().receive("ignore", () => console.log("auth error"))
-               .receive("ok", () => console.log("join ok"))
-               .after(10000, () => console.log("Connection interruption"))
-    chan.onError(e => console.log("something went wrong", e))
-    chan.onClose(e => console.log("channel closed", e))
-
-    $input.off("keypress").on("keypress", e => {
-      if (e.keyCode == 13) {
-        chan.push("new:msg", {user: $username.val(), body: $input.val()})
-        $input.val("")
-      }
-    })
-
-    chan.on("new:msg", msg => {
-      $messages.append(this.messageTemplate(msg))
-      scrollTo(0, document.body.scrollHeight)
-    })
-
-    chan.on("user:entered", msg => {
-      var username = this.sanitize(msg.user || "anonymous")
-      $messages.append(`<br/><i>[${username} entered]</i>`)
-    })
+    // var $status    = $("#status")
+    // var $messages  = $("#messages")
+    // var $input     = $("#message-input")
+    // var $username  = $("#username")
+    //
+    // socket.onOpen( ev => console.log("OPEN", ev) )
+    // socket.onError( ev => console.log("ERROR", ev) )
+    // socket.onClose( e => console.log("CLOSE", e))
+    //
+    // var chan = socket.channel("rooms:lobby", {})
+    // chan.join().receive("ignore", () => console.log("auth error"))
+    //            .receive("ok", () => console.log("join ok"))
+    //            .after(10000, () => console.log("Connection interruption"))
+    // chan.onError(e => console.log("something went wrong", e))
+    // chan.onClose(e => console.log("channel closed", e))
+    //
+    // $input.off("keypress").on("keypress", e => {
+    //   if (e.keyCode == 13) {
+    //     chan.push("new:msg", {user: $username.val(), body: $input.val()})
+    //     $input.val("")
+    //   }
+    // })
+    //
+    // chan.on("new:msg", msg => {
+    //   $messages.append(this.messageTemplate(msg))
+    //   scrollTo(0, document.body.scrollHeight)
+    // })
+    //
+    // chan.on("user:entered", msg => {
+    //   var username = this.sanitize(msg.user || "anonymous")
+    //   $messages.append(`<br/><i>[${username} entered]</i>`)
+    // })
   }
 
-  static sanitize(html){ return $("<div/>").text(html).html() }
-
-  static messageTemplate(msg){
-    let username = this.sanitize(msg.user || "anonymous")
-    let body     = this.sanitize(msg.body)
-
-    return(`<p><a href='#'>[${username}]</a>&nbsp; ${body}</p>`)
-  }
+  // static sanitize(html){ return $("<div/>").text(html).html() }
+  //
+  // static messageTemplate(msg){
+  //   let username = this.sanitize(msg.user || "anonymous")
+  //   let body     = this.sanitize(msg.body)
+  //
+  //   return(`<p><a href='#'>[${username}]</a>&nbsp; ${body}</p>`)
+  // }
 
 }
 
 $( () => App.init() )
+
+$( document ).ready(() => {
+  console.log("ready yoooo")
+
+  
+
+  let $edit_data_buttons = $( "button[id*='data-btnEdit-']" );
+  $edit_data_buttons.on("click", (e) => {
+    console.log(`clicked. target id: ${JSON.stringify(e.currentTarget.id)}`);
+    // Set the key/value input boxes to the text in the key
+    let keyIndex = e.currentTarget.value;
+    let keyText = $( `#data-key-${keyIndex}` ).text();
+    $( "#mut8n_key[name='mut8n[key]']" ).val(keyText);
+    let valueText = $( `#data-value-${keyIndex}` ).text();
+    $( "#mut8n_value[name='mut8n[value]']" ).val(valueText);
+  })
+  // $remove_data_buttons.each(x => {
+  //   console.log(JSON.stringify(x));
+  //
+  //   x.on("click", e => {
+  //     console.log(`clicked`);
+  //     // console.log(`clicked: ${x.id}`);
+  //   })
+  // });
+});
+
+
 
 export default App
