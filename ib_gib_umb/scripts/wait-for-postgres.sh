b59bf88be3ca40fb78727bf8463e370fe97e63f2
William Raiford <bill.mybiz@gmail.com>
2016-10-22 14:52:23 -0500

    *:boom::mouse: Tweaked wait-for-postgres.sh to use other env vars
    * Need to improve the whole secret handling business.
    
    attr: meta
    
    issue: relates to #74

--- ib_gib_umb/scripts/wait-for-postgres.sh
+++ ib_gib_umb/scripts/wait-for-postgres.sh
@@ -2,11 +2,49 @@
 
 set -e
 
+echo "1: $1"
 host="$1"
-shift
-cmd="$@"
+echo "host: $host"
+echo ""
 
-until psql -h "$host" -U "postgres" -c '\l'; do
+echo "2: $2"
+PORT="$2"
+echo "PORT: $port"
+echo ""
+
+echo "3: $3"
+PG_PORT="$3"
+echo "PG_PORT: $PG_PORT"
+echo ""
+
+echo "4: $4"
+POSTGRES_USER="$4"
+echo "POSTGRES_USER: $POSTGRES_USER"
+echo ""
+
+echo "5: $5"
+POSTGRES_PASSWORD="$5"
+echo "POSTGRES_PASSWORD: $POSTGRES_PASSWORD"
+echo ""
+
+echo "6: $6"
+app_bin="$6"
+echo ""
+
+echo "7: $7"
+foreground="$7"
+echo ""
+
+cmd="$app_bin $foreground"
+echo "cmd: $cmd"
+echo ""
+
+# echo "@ is"
+# echo "$@"
+
+export PGPASSWORD="$POSTGRES_PASSWORD"
+
+until psql -h "$host" -U $POSTGRES_USER -c '\l'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
