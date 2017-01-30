b11746ebaf8d84a535be045f78bd16ee8d479f8a
William Raiford <bill.mybiz@gmail.com>
2017-01-30 08:47:06 -0600

    ib_gib:fire: Increased max comment length to 16_384
    * I'm not really sure I even need this kind of arbitrary max, since
      we are already limiting pic size, etc. :thinking:
    
    attr: minor
    
    issue: closes #152

--- ib_gib_umb/apps/ib_gib/lib/ib_gib/constants.ex
+++ ib_gib_umb/apps/ib_gib/lib/ib_gib/constants.ex
@@ -87,7 +87,7 @@ defmodule IbGib.Constants do
       @min_email_addr_size 5
       @max_email_addr_size 63
       @regex_valid_email_addr ~r/^[\w][\w+\.]*(?<=[\w])[]@[\w][\w\.]*\.[\w]+$/
-      @max_comment_text_size 4096
+      @max_comment_text_size 16_384
       @min_link_text_size 10 # http://a.b
       @max_link_text_size 255
       @min_query_data_text_size 0
